1e244069df50dc02e922856c3134e39b05f79089
lucas.ripoche <lucas.ripoche@jaguar-network.com>
2020-04-25 12:31:48 +0200

    Added burger list and burger detail

---
+++ src/app/burger-list/burger-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-burger-list',
+  templateUrl: './burger-list.component.html',
+  styleUrls: ['./burger-list.component.css']
+})
+export class BurgerListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
